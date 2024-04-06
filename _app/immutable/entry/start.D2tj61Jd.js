961df992cf39729499730bcdfd9f1da0a8330a38
ZedeDev <26501287+Xarithma@users.noreply.github.com>
2024-04-06 22:53:40 +0200

    Updates

---
+++ _app/immutable/entry/start.D2tj61Jd.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BWYulPAh.js";export{t as start};
---
+++ _app/immutable/nodes/2.BDxmcL2j.js
@@ -0,0 +1,6 @@
+import{s as r,n as s}from"../chunks/scheduler.Cewbzyqj.js";import{S as i,i as l,e as m,c as o,g as p,a as d,b as x,d as f}from"../chunks/index.elqt4f1q.js";import{b as h}from"../chunks/paths.CNNvpYCp.js";function u(n){let e,a=`<section class="text-center"><h1 class="text-6xl flex item-center justify-center gap-2">Hi, I&#39;m
+			<span class="flex items-center bg-secondary rounded md:p-1 p-2 text-secondary-content">Zede.</span></h1> <h2 class="mt-6 text-lg">I&#39;m a
+			<span class="bg-accent p-1 rounded text-accent-content">game</span> &amp;
+			<span class="bg-primary p-1 rounded text-primary-content">web</span>
+			developer.</h2></section> <section class="flex flex-wrap gap-8 justify-center items-center mt-24 max-w-7xl"><p>Check out my game
+			<span><a href="${h}/spiredle" class="underline text-accent">Spiredle</a></span></p></section>`;return{c(){e=m("main"),e.innerHTML=a,this.h()},l(t){e=o(t,"MAIN",{class:!0,"data-svelte-h":!0}),p(e)!=="svelte-1kpktty"&&(e.innerHTML=a),this.h()},h(){d(e,"class","mx-16 md:mx-0 flex flex-col justify-center items-center min-h-[100vh]")},m(t,c){x(t,e,c)},p:s,i:s,o:s,d(t){t&&f(e)}}}class _ extends i{constructor(e){super(),l(this,e,null,u,r,{})}}export{_ as component};
