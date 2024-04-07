a51a8aac36ef83a52361e15d9bf30aebcef849f0
ZedeDev <26501287+Xarithma@users.noreply.github.com>
2024-04-07 19:46:45 +0200

    Add metadata to landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,9 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Zede.dev</title>
+	<meta name="description" content="Hi, I'm Zede! I'm a web and game developer." />
+</svelte:head>
+
 <slot />
