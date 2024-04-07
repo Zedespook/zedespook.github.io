a8d34a297325e80dee9c6b802f18bc20a35e8852
ZedeDev <26501287+Xarithma@users.noreply.github.com>
2024-04-07 16:32:09 +0200

    Add scoring system, difficulty options & daily streak counter

--- src/routes/spiredle/+page.svelte
+++ src/routes/spiredle/+page.svelte
@@ -1,32 +1,96 @@
 <script lang="ts">
-	import { cards } from '$lib';
-	import type { Card } from '$lib';
+	import Icon from '@iconify/svelte';
+
 	import { onMount } from 'svelte';
+
+	import type { Card } from '$lib';
+	import { cards } from '$lib';
+
+	let loading = true;
 
 	enum GameType {
 		DAILY,
 		RANDOM
 	}
 
+	enum GameState {
+		PLAYING,
+		WON,
+		LOST
+	}
+
 	let gameType = GameType.DAILY;
+	let gameState = GameState.PLAYING;
 
 	let cardToGuess = {} as Card;
 	let searchResults = [] as Card[];
 	let guesses = [] as Card[];
 
-	let isWon = false;
+	type SpiredleSettings = {
+		disableSearchImages: boolean;
+		disableZoomOnGuess: boolean;
+		disableHintOnGuess: boolean;
+		allowFiveGuessesOnly: boolean;
+	};
 
-	function getCardOfTheDay() {
+	let spiredleSettings: SpiredleSettings = {
+		disableSearchImages: false,
+		disableZoomOnGuess: false,
+		disableHintOnGuess: false,
+		allowFiveGuessesOnly: false
+	};
+
+	let score = 100;
+	let overallScore = 0;
+	let scoreMultiplier = 1.0;
+	let zoomLevel = 10;
+	let canChangeSettings = true;
+
+	$: {
+		if (!loading) {
+			if (spiredleSettings) {
+				localStorage.setItem('spiredleSettings', JSON.stringify(spiredleSettings));
+
+				scoreMultiplier = 1.0;
+
+				if (spiredleSettings.disableSearchImages) {
+					scoreMultiplier += 0.06;
+				}
+
+				if (spiredleSettings.disableZoomOnGuess) {
+					scoreMultiplier += 0.06;
+				}
+
+				if (spiredleSettings.disableHintOnGuess) {
+					scoreMultiplier += 0.12;
+				}
+
+				if (spiredleSettings.allowFiveGuessesOnly) {
+					scoreMultiplier += 0.12;
+				}
+			}
+
+			if (gameState === GameState.WON) {
+				localStorage.setItem('spiredleOverallScore', overallScore.toString());
+			}
+		}
+	}
+
+	function getSeed() {
 		const now = new Date();
 		const utcDate = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate());
-		const seed = utcDate.toISOString().slice(0, 10);
+		return utcDate.toISOString().slice(0, 10);
+	}
+
+	function getCardOfTheDay() {
+		const seed = getSeed();
 		const seedNumber = parseInt(seed.replace(/-/g, ''));
 		const randomIndex = Math.abs(seedNumber % cards.length);
 		return cards[randomIndex];
 	}
 
 	function searchCards(e: Event) {
-		if (isWon) return;
+		if (gameState === GameState.WON) return;
 
 		const searchTerm = (e.target as HTMLInputElement).value;
 
@@ -52,15 +116,67 @@
 				}
 			})
 			.slice(0, 5);
+
+		canChangeSettings = false;
 	}
 
 	function guessCard(card: Card) {
-		if (isWon) return;
+		if (gameState === GameState.WON) return;
 
 		guesses = [...guesses, card];
 
 		if (card === cardToGuess) {
-			isWon = true;
+			score *= scoreMultiplier;
+			overallScore += score;
+			gameState = GameState.WON;
+
+			if (gameType === GameType.DAILY) {
+				const dailySeedStore = localStorage.getItem('spiredleDailyCompleted');
+				const todaySeed = getSeed();
+
+				if (dailySeedStore && dailySeedStore === getPreviousDateSeed(todaySeed)) {
+					const streak = localStorage.getItem('spiredleDailyStreak');
+					localStorage.setItem('spiredleDailyStreak', String(Number(streak) + 1));
+				} else {
+					localStorage.setItem('spiredleDailyStreak', '1');
+				}
+
+				localStorage.setItem('spiredleDailyCompleted', getSeed());
+			}
+		} else if (guesses.length === 5 && spiredleSettings.allowFiveGuessesOnly) {
+			gameState = GameState.LOST;
+
+			if (gameType === GameType.DAILY) {
+				const dailySeedStore = localStorage.getItem('spiredleDailyCompleted');
+				const todaySeed = getSeed();
+
+				if (dailySeedStore && dailySeedStore === getPreviousDateSeed(todaySeed)) {
+					const streak = localStorage.getItem('spiredleDailyStreak');
+					localStorage.setItem('spiredleDailyStreak', String(Number(streak) + 1));
+				} else {
+					localStorage.setItem('spiredleDailyStreak', '1');
+				}
+
+				localStorage.setItem('spiredleDailyCompleted', getSeed());
+			}
+		} else {
+			if (cardToGuess.owner !== card.owner) {
+				score -= 2;
+			}
+
+			if (cardToGuess.energy !== card.energy) {
+				score -= 2;
+			}
+
+			if (cardToGuess.rarity !== card.rarity) {
+				score -= 2;
+			}
+
+			if (cardToGuess.type !== card.type) {
+				score -= 2;
+			}
+
+			score -= 2;
 		}
 
 		searchResults = [];
@@ -70,50 +186,110 @@
 		cardToGuess = cards[Math.floor(Math.random() * cards.length)];
 		searchResults = [];
 		guesses = [];
-		isWon = false;
+		gameState = GameState.PLAYING;
 		gameType = GameType.RANDOM;
+		score = 100;
+		canChangeSettings = true;
+	}
+
+	function getPreviousDateSeed(currentSeed: string): string {
+		const currentDate = new Date(currentSeed);
+		const previousDate = new Date(currentDate.getTime() - 24 * 60 * 60 * 1000);
+		return previousDate.toISOString().slice(0, 10);
+	}
+
+	function openSettings() {
+		const settingsModal = document.getElementById('settingsModal') as HTMLDialogElement;
+
+		if (settingsModal) {
+			settingsModal.showModal();
+		}
 	}
 
 	onMount(() => {
 		cardToGuess = getCardOfTheDay();
+
+		const settingsStore = localStorage.getItem('spiredleSettings');
+		if (settingsStore) {
+			spiredleSettings = JSON.parse(settingsStore);
+		}
+
+		const overallScoreStore = localStorage.getItem('spiredleOverallScore');
+		if (overallScoreStore) {
+			overallScore = parseFloat(overallScoreStore);
+		}
+
+		const dailySeedStore = localStorage.getItem('spiredleDailyCompleted');
+		if (dailySeedStore) {
+			if (getSeed() === dailySeedStore) {
+				gameType = GameType.RANDOM;
+				startNewGameWithRandomCard();
+			}
+		}
+
+		loading = false;
 	});
 </script>
 
-<div class="flex flex-col justify-center items-center mx-4 my-12 md:max-w-xl md:mx-auto">
+<main class="flex flex-col justify-center items-center mx-4 my-12 md:max-w-xl md:mx-auto">
 	<div class="flex flex-col text-center">
-		<h1 class="text-4xl font-bold">Spiredle (v2)</h1>
+		<div class="flex items-center justify-center text-center gap-4">
+			<h1 class="text-4xl font-bold">Spiredle</h1>
+			<button class="btn btn-primary" on:click={openSettings} disabled={!canChangeSettings}
+				><Icon icon="mdi:settings-outline" class="w-8 h-8" /></button
+			>
+		</div>
 		<p class="mt-4">Guess a card, get a hint after every try.</p>
 	</div>
+
+	<p class="my-4 font-bold text-lg">Your Overall Score: {overallScore.toFixed(2)}</p>
 
 	<div class="relative overflow-hidden mt-8">
 		<img
 			src={cardToGuess.image}
 			alt="Bro, why are you checking the source?"
-			class="h-full object-cover transform scale-[5] origin-[50%_25%]"
+			class="h-full object-cover transform origin-[50%_25%]"
+			style="scale: {zoomLevel}"
 		/>
 	</div>
-	<p class="font-bold italic">Guess the card on the image.</p>
 
-	{#if isWon}
+	<div class="flex justify-center items-center gap-4 my-8">
+		<button class="btn btn-primary" on:click={startNewGameWithRandomCard}>Guess Random Card</button>
+	</div>
+
+	{#if gameState === GameState.WON}
 		<div class="my-4">
 			<p class="text-green-500 text-xl font-bold text-center">You Win!</p>
+			<p class="text-green-500 text-center">
+				Final score: {score.toFixed(2)} (with {scoreMultiplier.toFixed(2)}x multiplier)
+			</p>
 			{#if gameType === GameType.DAILY}
 				<p class="text-green-500 font-bold text-center">
 					You've guessed the card of the day, but you can play again with a random card.
 				</p>
+				<p class="font-bold text-center">
+					Your daily streak is: {localStorage.getItem('spiredleDailyStreak')}
+				</p>
 			{/if}
 		</div>
-
-		<div class="flex justify-center items-center gap-4 my-8">
-			<button class="btn btn-primary" on:click={startNewGameWithRandomCard}
-				>Guess New Random Card</button
-			>
+	{:else if gameState === GameState.LOST}
+		<div class="my-4">
+			<p class="text-red-500 text-xl font-bold text-center">You Lost.</p>
+			{#if gameType === GameType.DAILY}
+				<p class="text-red-500 font-bold text-center">
+					Unfortunately you didn't guess the card of the day with your difficulty settings. Good
+					luck tomorrow!
+				</p>
+			{/if}
 		</div>
 	{:else}
-		<div class="flex justify-center items-center gap-4 mb-4 mt-8">
-			<button class="text-primary underline" on:click={startNewGameWithRandomCard}
-				>Guess Random Card</button
-			>
+		<div class="flex flex-col items-center justify-between gap-2 my-4">
+			<p class="text-sm">
+				Your score: {score.toFixed(0)} (with {scoreMultiplier.toFixed(2)}x multiplier)
+			</p>
+			{#if spiredleSettings.allowFiveGuessesOnly}
+				<p class="font-bold text-sm">Guesses left: {5 - guesses.length}</p>
+			{/if}
 		</div>
 
 		<div class="flex justify-center items-center gap-4 mb-8">
@@ -131,8 +307,11 @@
 			<li>
 				<button on:click={() => guessCard(card)} class="w-full">
 					<div class="h-24 p-2 bg-base-200 w-full flex items-center gap-2 rounded-lg border my-1">
-						<img src={card.image} alt={card.name} class="h-full object-cover" />
-						<p>{card.name}</p>
+						{#if !spiredleSettings.disableSearchImages}
+							<img src={card.image} alt={card.name} class="h-full object-cover" />
+						{/if}
+
+						<p class="ml-2">{card.name}</p>
 					</div>
 				</button>
 			</li>
@@ -142,38 +321,109 @@
 	<ul class="w-full my-8">
 		{#each guesses as card}
 			<li class="my-2">
-				<p class="text-center my-2">{card.name}</p>
-				<div class="flex justify-center items-center gap-2">
-					<div
-						class="p-4 w-20 h-20 text-center flex items-center text-xs justify-center rounded-lg"
-						class:bg-green-500={card.owner === cardToGuess.owner}
-						class:bg-red-500={card.owner !== cardToGuess.owner}
-					>
-						{card.owner}
+				<p class="text-center my-2">
+					{card.name}
+					{cardToGuess === card ? '(correct)' : '(incorrect)'}
+				</p>
+
+				{#if !spiredleSettings.disableHintOnGuess}
+					<div class="flex justify-center items-center gap-2">
+						<div
+							class="p-4 w-20 h-20 text-center flex items-center text-xs justify-center rounded-lg"
+							class:bg-green-500={card.owner === cardToGuess.owner}
+							class:bg-red-500={card.owner !== cardToGuess.owner}
+						>
+							{card.owner}
+						</div>
+						<div
+							class="p-4 w-20 h-20 text-center flex items-center text-xs justify-center rounded-lg"
+							class:bg-green-500={card.energy === cardToGuess.energy}
+							class:bg-red-500={card.energy !== cardToGuess.energy}
+						>
+							{card.energy}
+						</div>
+						<div
+							class="p-4 w-20 h-20 text-center flex items-center text-xs justify-center rounded-lg"
+							class:bg-green-500={card.rarity === cardToGuess.rarity}
+							class:bg-red-500={card.rarity !== cardToGuess.rarity}
+						>
+							{card.rarity}
+						</div>
+						<div
+							class="p-4 w-20 h-20 text-center flex items-center text-xs justify-center rounded-lg"
+							class:bg-green-500={card.type === cardToGuess.type}
+							class:bg-red-500={card.type !== cardToGuess.type}
+						>
+							{card.type}
+						</div>
 					</div>
-					<div
-						class="p-4 w-20 h-20 text-center flex items-center text-xs justify-center rounded-lg"
-						class:bg-green-500={card.energy === cardToGuess.energy}
-						class:bg-red-500={card.energy !== cardToGuess.energy}
-					>
-						{card.energy}
-					</div>
-					<div
-						class="p-4 w-20 h-20 text-center flex items-center text-xs justify-center rounded-lg"
-						class:bg-green-500={card.rarity === cardToGuess.rarity}
-						class:bg-red-500={card.rarity !== cardToGuess.rarity}
-					>
-						{card.rarity}
-					</div>
-					<div
-						class="p-4 w-20 h-20 text-center flex items-center text-xs justify-center rounded-lg"
-						class:bg-green-500={card.type === cardToGuess.type}
-						class:bg-red-500={card.type !== cardToGuess.type}
-					>
-						{card.type}
-					</div>
-				</div>
+				{/if}
 			</li>
 		{/each}
 	</ul>
-</div>
+</main>
+
+<dialog id="settingsModal" class="modal">
+	<div class="modal-box">
+		<h3 class="font-bold text-lg">Settings</h3>
+		<p class="text-sm italic mb-4">*You can only change settings before you start guessing.</p>
+
+		<div class="my-2">
+			<div class="form-control">
+				<label class="cursor-pointer label">
+					<span class="label-text">Disable images in search bar (+0.06x multiplier)</span>
+					<input
+						type="checkbox"
+						class="toggle toggle-primary"
+						bind:checked={spiredleSettings.disableSearchImages}
+					/>
+				</label>
+			</div>
+
+			<div class="form-control">
+				<label class="cursor-pointer label">
+					<span class="label-text">Disable zoom after every guess (+0.06x multiplier)</span>
+					<input
+						type="checkbox"
+						class="toggle toggle-primary"
+						bind:checked={spiredleSettings.disableZoomOnGuess}
+					/>
+				</label>
+			</div>
+
+			<div class="form-control">
+				<label class="cursor-pointer label">
+					<span class="label-text">Disable hints after every guess (+0.12x multiplier)</span>
+					<input
+						type="checkbox"
+						class="toggle toggle-primary"
+						bind:checked={spiredleSettings.disableHintOnGuess}
+					/>
+				</label>
+			</div>
+
+			<div class="form-control">
+				<label class="cursor-pointer label">
+					<span class="label-text">Allow 5 guesses only (+0.12x multiplier)</span>
+					<input
+						type="checkbox"
+						class="toggle toggle-primary"
+						bind:checked={spiredleSettings.allowFiveGuessesOnly}
+					/>
+				</label>
+			</div>
+		</div>
+
+		<div class="my-4">
+			<p class="font-bold">
+				Score multiplier: {scoreMultiplier.toFixed(2)}x
+			</p>
+		</div>
+
+		<div class="modal-action">
+			<form method="dialog">
+				<button class="btn mr-1">Close</button>
+			</form>
+		</div>
+	</div>
+</dialog>
